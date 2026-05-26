60a41843774517bc6715c7bb47508751e23b5a88
jozi <dev@jokimazi.site>
2026-05-27 00:55:48 +1000

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "site.jokimazi.fillimail"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0-Alpha"
+        versionCode = 4
+        versionName = "1.3.0-Alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
